4acbb279737b525a7f495b3177400d5f77b0bda6
shafiquerashid <shafique.a.rashid@gmail.com>
2016-01-06 19:54:52 -0800

    Created dashboardController.js

---
+++ client/app/dashboard/dashboardController.js
@@ -0,0 +1,21 @@
+angular.module('client-recon.dashboard', [])
+
+.controller('DashboardController', function ($scope, ClientsApi) {
+  // Your code here
+
+  $scope.data = {};
+
+  var initializeClients = function () {
+    ClientsApi.getAll()
+      .then(function (clients) {
+        console.log(clients);
+        $scope.data.clients = clients;
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+  };
+  };
+
+  initializeClients();
+  });
