249f68e323d5a21afd5acba07bb940fc1e9e2dcf
Alan Fu <alanzfu@gmail.com>
2016-01-06 21:40:39 -0800

    Creates JOIN table schema for salesperson_client;

--- db/schema.js
+++ db/schema.js
@@ -1,6 +1,15 @@
 var db = require('./config.js');
 
 
+db.query("SELECT * FROM client INNERJOIN salesperson_client on (salesperson_client.client_id = client.client_id) WHERE salesperson_client.salesperson_id = 1")
+.then(function(data){
+  console.log('success');
+  console.log(data);
+})
+.catch(function(error){
+  console.log(error);
+});
+
 //SCHEMAS ---------------------------------------------
 //Cannot use USER as a table name || forbidden words: http://www.postgresql.org/docs/current/interactive/sql-keywords-appendix.html
 db.query("CREATE TABLE IF NOT EXISTS salesperson (salesperson_name VARCHAR(40),salesperson_id SERIAL PRIMARY KEY);")
@@ -11,7 +20,7 @@ db.query("CREATE TABLE IF NOT EXISTS salesperson (salesperson_name VARCHAR(40),s
  })
  .then(function(){
    console.log('client created');
-   return db.query("CREATE TABLE IF NOT EXISTS salesperson_client (salesperson_id int REFERENCES salesperson (salesperson_id) ON UPDATE CASCADE ON DELETE CASCADE,client_id int REFERENCES client (client_id) ON UPDATE CASCADE ON DELETE CASCADE)");
+   return db.query("CREATE TABLE IF NOT EXISTS salesperson_client (salesperson_id int NOT NULL, client_id int NOT NULL, PRIMARY KEY (salesperson_id,client_id), FOREIGN KEY (salesperson_id) REFERENCES salesperson(salesperson_id), FOREIGN KEY (client_id) REFERENCES client(client_id));")
  })
  .then(function(){
    console.log('user client join created');
