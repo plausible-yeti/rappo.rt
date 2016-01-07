5d1d7c670cc2218e891d4a879d6e2ad144b8f859
Rebecca Yang <rebeccayang123@gmail.com>
2016-01-06 20:52:36 -0800

    Adds routes and controller functionality for all get, post, put requests taht we have spec'd out

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -2,9 +2,30 @@ module.exports = {
 	dashboard: {
 		get: function(req,res){
 			var userid = req.params.user_id
-			// This is where I call the dashboard function
+			// This is where we call the DB function for getting dashboard
 			// TO DO: Make sure you send a response
 			
+		}
+	},
+	user: {
+		post: function(req,res){
+			// This is where we call the DB function for creating new user
+			// TO DO: Make sure you send a response
+		}	
+	},
+	client: {
+		get: function(req,res){
+			var userid = req.params.user_id;
+			var clientid = req.params.client_id;
+			// This is where we call the DB function for getting particualr client
+		},
+		post: function(req,res){
+			var userid = req.params.user_id;
+			// This is where we call the DB function for creating a new client
+		},
+		put: function(req,res){
+			var user_id = req.params.user_id;
+			// This is where we call the DB function for updating a client
 		}
 	}
 }
--- server/routes/index.js
+++ server/routes/index.js
@@ -7,8 +7,28 @@ module.exports = function(app, express) {
 	});
 
 	// ROUTE FOR DISPLAYING DASHBOARD
-	app.get('/api/users/:user_id/clients',controller.dashboard.get(req,res));
+	app.get('/api/users/:user_id/clients', function(req,res){
+		controller.dashboard.get(req,res);
+	});
 
+	// ROUTE FOR CREATING A NEW USER
+	app.post('/api/users', function(req,res){
+		 controller.user.post(req,res);
+	});
+
+	// ROUTE FOR DISPLAYING PARTICULAR CLIENT
+	app.get('/api/users/:user_id/clients/:client_id', function(req,res){
+		controller.client.get(req,res);
+	})
+	
+	// ROUTE FOR CREATING A NEW CLIENT
+	app.post('/api/users/:user_id/clients', function(req,res){
+		controller.client.post(req,res);
+	})
+
+	app.put('/api/users/:user_id/clients', function(req,res){
+		controller.client.put(req,res);
+	})
 	
 }
 
