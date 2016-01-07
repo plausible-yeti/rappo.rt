f83eea49a4d26ee9c987c251fbc56b0fe898f3f8
Rebecca Yang <rebeccayang123@gmail.com>
2016-01-06 20:37:26 -0800

    Sets up basic server and handles routing for '/api/users/:user_id/clients'

---
+++ server/controllers/index.js
@@ -0,0 +1,10 @@
+module.exports = {
+	dashboard: {
+		get: function(req,res){
+			var userid = req.params.user_id
+			// This is where I call the dashboard function
+			// TO DO: Make sure you send a response
+			
+		}
+	}
+}
