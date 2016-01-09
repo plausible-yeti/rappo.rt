b58cd7e6eab2be0e705377e538f6073fbf216529
Rebecca Yang <rebeccayang123@gmail.com>
2016-01-09 10:55:45 -0800

    Builds out new client HTML template. Builds out client API function: addOne, which handles POST request. Builds out newClientController.

---
+++ client/app/new-client/newClientController.js
@@ -0,0 +1,40 @@
+// TO DO:
+// 1. Detect clientID from the AppState and put it in the ClientsApi.addOne()
+
+angular.module('client-recon.newclient', [])
+
+.controller('NewClientController', function ($scope, $timeout, ClientsApi, AppState) {
+  // TEMPLATE FOR DATA
+  this.data = {
+    name:'',
+    email: '',
+    birthday: '',
+    company: ''
+  };
+
+  var successfulPost = this.success;
+
+  var newClient = this.data;
+
+  this.postClient = function () {
+
+    ClientsApi.addOne( ***** CLIENT ID *******,newClient).then(function(res){
+      // CALLED AFTER SUCCESSFUL POST
+      successfulPost = true;
+      newClient.name = '';
+      newClient.email = '';
+      newClient.birthday = '';
+      newClient.company = '';
+
+      // THE PURPOSE OF THE BELOW IS TO HAVE THE SUCCESSFUL POST 
+      // NOTIFICATION ONLY SHOW FOR A FEW SECONDS AND DISAPPEAR
+      $timeout(function(){
+        successfulPost = false;
+      }, 2000);
+
+    })
+
+  };
+
+
+  });
