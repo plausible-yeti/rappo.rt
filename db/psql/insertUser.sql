86c8b3f031a0a94adb52bd208a00f56fe6941f9f
Rebecca Yang <rebeccayang123@gmail.com>
2016-01-10 16:14:53 -0800

    Updates insertUser.sql to add to join table

--- db/psql/insertUser.sql
+++ db/psql/insertUser.sql
@@ -1,2 +1,5 @@
 INSERT INTO clients (client_name, client_email, client_birthday, client_company, client_zipcode, client_title, client_image) 
 VALUES ($1, $2, $3, $4, $5, $6, $7);
+
+INSERT INTO clients_salespersons (user_id, client_id)
+VALUES ($8, SELECT (client_id) WHERE client_name = $1);
